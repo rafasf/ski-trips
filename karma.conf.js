5552f2c979140dde4771e601fb4f3938d2bea38d
Rafael Ferreira <rafaelsf@gmail.com>
2017-04-23 16:28:01 -0500

    use selenium/node-chrome for CI

--- karma.conf.js
+++ karma.conf.js
@@ -38,7 +38,19 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    browsers: ['ChromeHeadless'],
+    singleRun: false,
+
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--no-sandbox',
+          '--headless',
+          '--disable-gpu',
+          ' --remote-debugging-port=9222',
+        ],
+      }
+    }
   });
 };
